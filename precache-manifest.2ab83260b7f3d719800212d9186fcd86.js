1525feac67d28dd76caf1155ebfaa0d0e681e59a
Saurabh Takle <41586938+saurabhtakle@users.noreply.github.com>
2020-08-04 17:02:22 +0530

    Updates

---
+++ precache-manifest.2ab83260b7f3d719800212d9186fcd86.js
@@ -0,0 +1,74 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a52df2625297d3c77397e6504f150e8f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "d7d02de24aa2ba96cfeb",
+    "url": "/static/css/main.69eda509.chunk.css"
+  },
+  {
+    "revision": "8a0072e30ba73121838b",
+    "url": "/static/js/2.99269e70.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/static/js/2.99269e70.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d7d02de24aa2ba96cfeb",
+    "url": "/static/js/main.884d112c.chunk.js"
+  },
+  {
+    "revision": "ca5b60486ed3f4651ffa",
+    "url": "/static/js/runtime-main.42c3d899.js"
+  },
+  {
+    "revision": "64aaeb1642cfcfb42a075db1fe9994da",
+    "url": "/static/media/bootstrap.64aaeb16.png"
+  },
+  {
+    "revision": "3a80c3439de11bdd168e8962011beeca",
+    "url": "/static/media/cp.3a80c343.png"
+  },
+  {
+    "revision": "8f42e018a9340b6e4e20161b06525308",
+    "url": "/static/media/cpp.8f42e018.png"
+  },
+  {
+    "revision": "9b9b0f98799cd792a166cab76e3c08df",
+    "url": "/static/media/css3.9b9b0f98.png"
+  },
+  {
+    "revision": "96edbc3e78456b5cd548ba6fe92e0b5e",
+    "url": "/static/media/html5.96edbc3e.png"
+  },
+  {
+    "revision": "96643270fe346cc83165128dfcfccd4c",
+    "url": "/static/media/linux.96643270.png"
+  },
+  {
+    "revision": "3968842212ba1df6ea3149f60ed7b18d",
+    "url": "/static/media/mysql.39688422.png"
+  },
+  {
+    "revision": "f3337980365a420c41c39bce325a2a5d",
+    "url": "/static/media/photoshop.f3337980.png"
+  },
+  {
+    "revision": "8eefa0a844a19d1fcb7c2894fb332acc",
+    "url": "/static/media/python.8eefa0a8.png"
+  },
+  {
+    "revision": "db6aae0ddad69b0b592b6d1e74bdcf06",
+    "url": "/static/media/tf.db6aae0d.png"
+  },
+  {
+    "revision": "0b5cf10ee95c69a3902e40c28fd27ce1",
+    "url": "/static/media/twitter.0b5cf10e.jpg"
+  },
+  {
+    "revision": "dacd12dbbea350b8823dd7c294382ac7",
+    "url": "/static/media/website_image.dacd12db.jpg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.8266d684a0b9e96822e906cf435f94a1.js"
+  "/precache-manifest.2ab83260b7f3d719800212d9186fcd86.js"
 );
 
 self.addEventListener('message', (event) => {
